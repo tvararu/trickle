080d2e5b8179a88ce77ed6d5b444666da3a03ab4
Theodor Vararu <theo@vararu.org>
2015-08-03 01:12:35 +0100

    Remove insecure.

--- server/main.js
+++ server/main.js
@@ -1,6 +1,18 @@
 Incomes = new Mongo.Collection('incomes')
 Expenses = new Mongo.Collection('expenses')
 
+Incomes.allow({
+  insert: function (userId, doc) {
+    return userId === doc.userId
+  }
+})
+
+Expenses.allow({
+  insert: function (userId, doc) {
+    return userId === doc.userId
+  }
+})
+
 Meteor.publish('incomes', function () {
   return Incomes.find({ userId: this.userId })
 })
