0681b95d18716a08d27b4ac5334fcd9efe137964
Theodor Vararu <theo@vararu.org>
2015-09-20 21:05:56 +0100

    Add fastclick.

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
 import App from 'components/App'
 
+// Fastclick.
+import injectTapEventPlugin from 'react-tap-event-plugin'
+injectTapEventPlugin()
+
 React.render(<App />, document.getElementById('root'))
