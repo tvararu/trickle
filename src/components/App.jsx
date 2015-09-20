02dee76fb5cbf21cbe47712e095affe238927e4f
Theodor Vararu <theo@vararu.org>
2015-09-20 18:30:28 +0100

    Add webpack and google authentication.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,54 @@
-import React, { Component } from 'react'
+import React from 'react'
+import Firebase from 'firebase'
 
-export class App extends Component {
+const ref = new Firebase('https://trickle.firebaseio.com')
+
+export default React.createClass({
+  displayName: 'App',
+  getInitialState () {
+    return {
+      authData: ref.getAuth(),
+      error: false
+    }
+  },
+  componentDidMount () {
+    ref.onAuth(this.authDataCallback)
+  },
+  authDataCallback (authData) {
+    this.setState({ authData })
+  },
+  login () {
+    ref.authWithOAuthPopup('google', (error) => {
+      if (error) {
+        this.setState({ error })
+      }
+    })
+  },
+  logout () {
+    ref.unauth()
+  },
+  isLoggedIn () {
+    return this.state.authData
+  },
+  renderLogin () {
+    if (this.isLoggedIn()) {
+      return <div onClick={ this.logout }>Logout</div>
+    } else {
+      return <div onClick={ this.login }>Login with Google</div>
+    }
+  },
+  renderApp () {
+    const { authData } = this.state
+    if (this.isLoggedIn()) {
+      return <div>Hello { authData.google.displayName }.</div>
+    } else {
+      return null
+    }
+  },
   render () {
-    return <div>Hello world</div>
+    return <div>
+      { this.renderLogin() }
+      { this.renderApp() }
+    </div>
   }
-}
+})
